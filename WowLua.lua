e9fd52ec4825c57286cec50e39c9d0e83fa1fd88
Hendrik Leppkes <h.leppkes@gmail.com>
2018-04-20 17:38:16 +0200

    Fix PlaySound usage

--- WowLua.lua
+++ WowLua.lua
@@ -248,7 +248,7 @@ end
 function WowLua:Initialize(frame)
 	WowLua:OnSizeChanged(frame)
 	table.insert(UISpecialFrames, "WowLuaFrame")
-	PlaySound("SOUNDKIT.IG_MAINMENU_OPEN");
+	PlaySound(SOUNDKIT.IG_MAINMENU_OPEN);
 	self:UpdateButtons()
 end
 
